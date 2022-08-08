5270871b515d1a457bf97ecd7e7c61353b00958e
Brian McNamara <brian.mcnamara@salesforce.com>
2022-08-08 09:24:41 -0700

    Example of IDEA-298956

--- src/main/kotlin/com/github/brianmcnamara/plugindemo/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/brianmcnamara/plugindemo/listeners/MyProjectManagerListener.kt
@@ -1,16 +1,13 @@
 package com.github.brianmcnamara.plugindemo.listeners
 
-import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import com.github.brianmcnamara.plugindemo.services.MyProjectService
+import org.jetbrains.jps.incremental.BuilderService
+import org.jetbrains.jps.service.JpsServiceManager
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
     override fun projectOpened(project: Project) {
-        project.service<MyProjectService>()
-
-        System.getenv("CI")
-            ?: TODO("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+        JpsServiceManager.getInstance().getExtensions(BuilderService::class.java)
     }
 }
--- src/main/kotlin/com/github/brianmcnamara/plugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/brianmcnamara/plugindemo/services/MyApplicationService.kt
@@ -1,13 +1,8 @@
 package com.github.brianmcnamara.plugindemo.services
 
-import com.github.brianmcnamara.plugindemo.MyBundle
-
 class MyApplicationService {
 
     init {
-        println(MyBundle.message("applicationService"))
 
-        System.getenv("CI")
-            ?: TODO("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
 }
